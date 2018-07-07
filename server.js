25add843d68a11cd8009ae1f0656b5375288c480
lironh <lironenk@gmail.com>
2018-07-07 10:54:37 +0300

    feat(oauth)added auth0 integration

--- server.js
+++ server.js
@@ -4,18 +4,49 @@ const Hapi = require('hapi');
 const mongoose = require('mongoose');
 const routes = require('./api/routes');
 const config = require('./conf.json');
+const auth0Config = require('./conf.json').auth0;
+const hapiAuthJWT = require('hapi-auth-jwt2');
+const jwksRsa = require('jwks-rsa');
 
 const server = Hapi.server({
   port: config.port,
   host: config.host
 });
 
+const validateUser = (decoded, request, callback) => {
+  console.log("Decoded", decoded);
+  if (decoded && decoded.sub) {
+    return callback(null, true, {});
+  }
+  return callback(null, false, {});
+};
+
 const init = async () => {
   mongoose.connect(config.db)
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.error(err));
+
+  await server.register(hapiAuthJWT);
+  server.auth.strategy('jwt', 'jwt', {
+    key: jwksRsa.hapiJwt2Key({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `${auth0Config.domain}/.well-known/jwks.json`
+    }),
+    verifyOptions: {
+      audience: `${auth0Config.audience}`,
+      issuer: `${auth0Config.domain}`,
+      algorithms: ['RS256']
+    },
+    validate: validateUser
+  });
+
+  server.auth.default('jwt');
+
+
   await server.start();
-  console.log(`Server running at: ${server.info.uri}`);
+  return server;
 };
 
 process.on('unhandledRejection', (err) => {
@@ -26,4 +57,9 @@ process.on('unhandledRejection', (err) => {
 
 server.route(routes);
 
-init();
+init()
+  .then((server) => {
+    console.log(`Server running at: ${server.info.uri}`);
+  }).catch(err => console.log(err)
+
+);
