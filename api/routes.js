a8daa34e34e3f8572aa5c6d36da47c8a4a8854e7
lironh <lironenk@gmail.com>
2018-08-04 10:05:36 +0300

    :hammer: wired routes

--- api/routes.js
+++ api/routes.js
@@ -1,7 +1,11 @@
 const provider = require('./provider/routes');
 const consumers = require('./consumers/routes');
+const serviceType = require('./type-of-service/routes');
+const subscription = require('./subscription/routes');
 
 module.exports = [
   ...provider,
-  ...consumers
+  ...consumers,
+  ...serviceType,
+  ...subscription
 ];
