1f3dd6b8e322a5f9f0df5930ebd07af7e4e2ace1
lironh <lironenk@gmail.com>
2018-11-29 12:41:50 +0200

    :hammer: removed old rest api for teachers

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,10 +1,8 @@
-const teacher = require('./teachers/routes');
 const students = require('./students/routes');
 const classType = require('./class-type/routes');
 const subscription = require('./subscription/routes');
 
 module.exports = [
-  ...teacher,
   ...students,
   ...classType,
   ...subscription
